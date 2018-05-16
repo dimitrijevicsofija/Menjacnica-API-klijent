67cc276c16a0d19368804c10d499f52d04aa554e
Sofija Dimitrijevic <dimitrijevic.sofijaa@gmail.com>
2018-05-16 17:06:43 +0200

    Izmenjena SOSacuvajKonverziju tako da u log.json-u bude upisan JSON niz.

--- src/menjacnica/sistemskeoperacije/SOSacuvajKonverziju.java
+++ src/menjacnica/sistemskeoperacije/SOSacuvajKonverziju.java
@@ -1,5 +1,6 @@
 package menjacnica.sistemskeoperacije;
 
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -7,11 +8,13 @@ import java.util.Date;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
 import menjacnica.Konverzija;
 
 public class SOSacuvajKonverziju {
-	
+
 	public static void izvrsi(String izValuta, String uValuta, double kurs) {
 		Date datumVreme = new Date();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSSSS");
@@ -24,10 +27,28 @@ public class SOSacuvajKonverziju {
 		k.setKurs(kurs);
 
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+		String object = gson.toJson(k);
+		JsonObject jsonk = gson.fromJson(object, JsonObject.class);
+
+		JsonArray istorijaKonverzija = null;
+
 		FileWriter writer;
+		FileReader reader;
+
 		try {
+			reader = new FileReader("log.json");
+			istorijaKonverzija = gson.fromJson(reader, JsonArray.class);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		try {
+			if (istorijaKonverzija == null)
+				istorijaKonverzija = new JsonArray();
+			istorijaKonverzija.add(jsonk);
+
 			writer = new FileWriter("log.json", true);
-			writer.write(gson.toJson(k));
+			writer.write(gson.toJson(istorijaKonverzija));
 			writer.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
